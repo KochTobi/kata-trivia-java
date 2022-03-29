67df3d45d08dc5cbfbf639b9ef5a98d5b8eefc8b
Steffengreiner <Steffen.Greiner@gmx.de>
2022-03-29 10:19:19 +0200

    Andreas drive

--- src/main/java/trivia/Player.java
+++ src/main/java/trivia/Player.java
@@ -3,18 +3,36 @@ package trivia;
 public class Player {
   
   private final String name;
-  private int place;
+
+  private int position;
+  private int money;
   
-  private Player(String name) {
+  private Player(String name, int position, int money) {
     this.name = name;
   }
   
   public static Player create(String name) {
-    return new Player(name);
+    return new Player(name, 0, 0);
   }
 
   public String getName() {
     return name;
+  }
+
+  public int getMoney() {
+    return money;
+  }
+
+  public void addMoney(int money) {
+    this.money += money;
+  }
+
+  public int getPosition() {
+    return position;
+  }
+
+  public void setPosition(int position) {
+    this.position = position;
   }
   
 }
